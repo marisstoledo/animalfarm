db10738cd54b84d046c7f7c008ec2c3bd6de79d3
marisstoledo <toledoma@g.crookcounty.orggit config --global marisstoledogit config --global user.email toledoma@g.crookcounty.org>
2021-05-06 13:45:00 -0700

    fixing render

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ function Animal(animalName){
     this.animalName = animalName;
    
 }
-Animal.prototype.render = function() {
-let animalDiv = document.createdElement('div');
-animalDiv.className = `${this.animalName}Object`;
+Animal.prototype.render = function(){
+let animalDiv = document.createElement('div');
+animalDiv.className = `${this.animalName}object`;
 animalDiv.textContent = this.animalName;
-let animalSection = documnet.querySelector(`#${this.animalName}Section`)
+let animalSection = document.querySelector(`#${this.animalName}Section`);
 animalSection.appendChild(animalDiv);
 }
     
-let makeCatButton  = document.getElementById('makeCat');
- makeCatButton.addEventListener('click', goMakeCat)
- function goMakeCat() {
+let makeCatButton = document.getElementById('makeCat');
+ makeCatButton.addEventListener('click', makeCat)
+ function makeCat(){
 let cat = new Animal('cat');
  cat.render();
 }
 
- let makeDogButton = document.getElementById('makeDog');
+ let makeDogButton = document.getElementById('goMakeDog');
 makeDogButton.addEventListener('click', goMakeDog)
 function goMakeDog() {
  let dog = new Animal('dog');
@@ -27,7 +27,7 @@ function goMakeDog() {
 }
 
 
-let makeHorseButton = document.getElementById('makeHorse');
+let makeHorseButton = document.getElementById('goMakeHorse');
 makeHorseButton.addEventListener('click', goMakeHorse)
 function goMakeHorse() {
     let horse = new Animal('horse');
@@ -35,7 +35,7 @@ function goMakeHorse() {
 }
 
 
-let makesheepButton = document.getElementById('makeSheep');
+let makesheepButton = document.getElementById('goMakeSheep');
 makesheepButton.addEventListener('click', goMakeSheep)
 function goMakeSheep() {
     let sheep = new Animal('sheep');
