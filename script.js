c30f29add25f3f1feaf4cced69d76005d276b3d4
marisstoledo <toledoma@g.crookcounty.orggit config --global marisstoledogit config --global user.email toledoma@g.crookcounty.org>
2021-05-06 12:58:35 -0700

    added java

--- script.js
+++ script.js
@@ -1,53 +1,43 @@
-'use strict'
+'use strict' 
 
-
- //TODO: I want to create a template - constructor function for an animal
-// Is that four different constructors?
-// Create a single constructor - template
-// the constructor takes as parameters - animalName
-// property called - animalName
-// method to render itself on the screen - render()
-
-function Animal(animalName, animalClass, animalId){
+function Animal(animalName){
     this.animalName = animalName;
-    this.animalClass = animalClass;
-    this.animalId = animalId;
-
+   
 }
 Animal.prototype.render = function() {
-let target = document.getElementById(this.animalId)
-let animalDiv = document.createdElement('div')
-animalDiv.className = this.animalName;
+let animalDiv = document.createdElement('div');
+animalDiv.className = `${this.animalName}Object`;
 animalDiv.textContent = this.animalName;
-animalSelection.appendChild(animalDiv);
+let animalSection = documnet.querySelector(`#${this.animalName}Section`)
+animalSection.appendChild(animalDiv);
 }
     
-    let makeCatButton  = document.getElementById('makeCat');
-    makeCatButton.addEventListener('click', goMakeCat)
-    function goMakeCat(){
-    let cat = new animal('cat', 'catObject','cat');
-    cat.render();
+let makeCatButton  = document.getElementById('makeCat');
+ makeCatButton.addEventListener('click', goMakeCat)
+ function goMakeCat() {
+let cat = new Animal('cat');
+ cat.render();
 }
 
  let makeDogButton = document.getElementById('makeDog');
 makeDogButton.addEventListener('click', goMakeDog)
-function goMakeDog(){
-    let dog = new Animal('dog', 'dogObject', 'dog');
-    dog.render();
+function goMakeDog() {
+ let dog = new Animal('dog');
+ dog.render();
 }
 
 
 let makeHorseButton = document.getElementById('makeHorse');
 makeHorseButton.addEventListener('click', goMakeHorse)
-function goMakeHorse(){
-    let dog = new Animal('horse', 'horseObject', 'horse');
+function goMakeHorse() {
+    let horse = new Animal('horse');
     horse.render();
 }
 
 
 let makesheepButton = document.getElementById('makeSheep');
-makeSheepButton.addEventListener('click', goMakeSheep)
-function goMakeSheep(){
-    let dog = new Animal('sheep', 'sheepObject', 'sheep');
+makesheepButton.addEventListener('click', goMakeSheep)
+function goMakeSheep() {
+    let sheep = new Animal('sheep');
     sheep.render();
 }
